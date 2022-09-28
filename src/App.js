8018772f0c916cc3f3c184804a74d608f059dafa
franjmr0416 <franjmr16@gmail.com>
2022-09-28 13:54:53 -0500

    validacion formulario

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useFormik } from "formik";
 
-function App() {
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.name) {
+    errors.name = "Requerido";
+  } else if (values.name.length < 5) {
+    errors.name = "El nombre es muy corto";
+  }
+  if (!values.lastname) {
+    errors.lastname = "Requerido";
+  } else if (values.lastname.length < 5) {
+    errors.lastname = "El apellido es muy corto";
+  }
+  return errors;
+};
+
+const App = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      lastname: "",
+      email: "",
+    },
+    validate: validate,
+    onSubmit: (values) => console.log(values),
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={formik.handleSubmit}>
+      <label>Nombre</label>
+      <input
+        name="name"
+        type="text"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.name}
+      />
+      {formik.touched.name && formik.errors.name ? (
+        <div>{formik.errors.name}</div>
+      ) : null}
+      <br />
+      <label>Apellido</label>
+      <input
+        name="lastname"
+        type="text"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.lastname}
+      />
+      {formik.touched.lastname && formik.errors.lastname ? (
+        <div>{formik.errors.lastname}</div>
+      ) : null}
+      <br />
+      <label>Correo</label>
+      <input
+        name="email"
+        type="text"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.email}
+      />
+      {formik.touched.email && formik.errors.email ? (
+        <div>{formik.errors.email}</div>
+      ) : null}
+      <button type="submit">Enviar</button>
+    </form>
   );
-}
+};
 
 export default App;
